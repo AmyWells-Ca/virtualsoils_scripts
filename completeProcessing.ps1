aabb2be672282950d4f82b82042d155a114dc866
Wells <amy.wells@ubc.ca>
2026-06-11 13:34:20 -0700

    Functionality for EML Workstation

--- completeProcessing.ps1
+++ completeProcessing.ps1
@@ -35,13 +35,16 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
 
             # "-importGroundControlPoints `"$PSScriptRoot\$ver\ControlPoints.csv`" `"$PSScriptRoot\$ver\ControlPointSettings.xml`"",
             # "-setReconstructionRegion `"$PSScriptRoot\$ver\reconstructionRegion.rsbox`"",
+        #
+        # Colmap
+        #
         
-
-        <#
         ## Settings for RealityScan
         $argsRealityScan = @(
             "-addFolder $projectDir\input\",
             "-setProjectCoordinateSystem Local:1",
+            "-detectMarkers `"$PSScriptRoot\reality_scan\36h11.xml`""
+            "-detectMarkers `"$PSScriptRoot\reality_scan\36h11.xml`""
             "-detectMarkers `"$PSScriptRoot\reality_scan\36h11.xml`""
             "-defineDistance `"$PSScriptRoot\reality_scan\Constraints.csv`"",
             "-align",
@@ -53,46 +56,48 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
             "-exportSelectedModel $projectDir\HP_$fieldName.fbx `"$PSScriptRoot\reality_scan\Export_FBX.xml`"",
             "-save `"$projectDir\reality_scan\RS_$fieldName.rsproj`"",
             "-quit"
-            )
-        #>
+        )
+        
+        Write-Host $argsRealityScan
 
-        # Start-Process -FilePath "F:\UnrealEngine\RealityScan_2.0\RealityScan.exe" -ArgumentList $argsRealityScan -Wait
+        Start-Process -FilePath "C:\Program Files\Epic Games\RealityScan_2.1\RealityScan.exe" -ArgumentList $argsRealityScan -Wait
+        
 
         #
         # 3DGS
         #
         
         # Settings for the "high quality" models
-        $argsLichtfeldStudio = @(
+        $argsLichtfeldStudioHigh = @(
             "-d `"$projectDir\alignment`"",
             "-o `"$projectDir\3DGS_High`"",
             "--config=`"$PSScriptRoot\lichtfeld_studio\lfs_config.json`""
         )
 
-        # Start-Process -FilePath "C:\Users\amys2001\LichtFeld-Studio\bin\LichtFeld-Studio.exe" -ArgumentList $argsLichtfeldStudio -WorkingDirectory "C:\Users\amys2001\LichtFeld-Studio" -Wait -WindowStyle Maximized
+        Start-Process -FilePath "C:\Users\amys2001\LichtFeld-Studio\bin\LichtFeld-Studio.exe" -ArgumentList $argsLichtfeldStudioHigh -WorkingDirectory "C:\Users\amys2001\LichtFeld-Studio" -Wait -WindowStyle Maximized
 
         # Settings for the "low quality" models
-        $argsLichtfeldStudio = @(
+        $argsLichtfeldStudioLow = @(
             "-d `"$projectDir\alignment`"",
             "-o `"$projectDir\3DGS_Low`"",
             "--config=`"$PSScriptRoot\lichtfeld_studio\lfs_config_low.json`""
         )
 
-        # Start-Process -FilePath "C:\Users\amys2001\LichtFeld-Studio\bin\LichtFeld-Studio.exe" -ArgumentList $argsLichtfeldStudio -WorkingDirectory "C:\Users\amys2001\LichtFeld-Studio" -Wait -WindowStyle Maximized
+        Start-Process -FilePath "C:\Users\amys2001\LichtFeld-Studio\bin\LichtFeld-Studio.exe" -ArgumentList $argsLichtfeldStudioLow -WorkingDirectory "C:\Users\amys2001\LichtFeld-Studio" -Wait -WindowStyle Maximized
 
-        <#
-        Write-Host "Editing 3DGS"
-        Write-Host ""
+        #
+        # Splat Editing & SOG generation
+        #
 
-        cd "$projectDir" # Sets directory to the project directory as splat-transform needs to be run in that directory.
-        $highPly = "$projectDir\3DGS\splat_60000.ply"
-        $lowPly = "$projectDir\3DGS\splat_75000.ply"
-        splat-transform $highPly $fieldName"_Blender.ply" # Just copies the .PLY hehe
-        splat-transform $highPly --filter-sphere "0,0,0,255" $fieldName"_WebHigh.ply" # Crops beyond 255m from origin
-        splat-transform $highPly --filter-sphere "0,0,0,255" $fieldName"_WebHigh.sog"
-        splat-transform $lowPly --filter-sphere "0,0,0,255" $fieldName"_WebLow.ply"
-        splat-transform $lowPly --filter-sphere "0,0,0,255" $fieldName"_WebLow.sog"
-        #>
+        cd $projectDir # Sets directory to the project directory as splat-transform needs to be run in that directory.
+
+        $plyHigh = $projectDir+"\3DGS_High\splat_135000.ply"
+        $plyLow = $projectDir+"\3DGS_Low\splat_75000.ply"
+        
+        splat-transform $plyHigh --filter-sphere "0,0,0,255" $fieldName"_HighQuality.ply" # Crops beyond 255m from origin
+        splat-transform $plyHigh --filter-sphere "0,0,0,255" $fieldName"_HighQuality.sog"
+        splat-transform $plyLow --filter-sphere "0,0,0,127" $fieldName"_LowQuality.ply"
+        splat-transform $plyLow --filter-sphere "0,0,0,127" $fieldName"_LowQuality.sog"
 
         #
         # Metadata Companion File
@@ -128,17 +133,16 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
             }
         }
 
+        <#
         $Shell = New-Object -ComObject Shell.Application
         $Folder = $Shell.Namespace($projectDir+'\input\')
         $File = $Folder.ParseName( (Get-ChildItem -Path $projectDir'\input\' -File | Select-Object -First 1) )
 
         $modelProperties.metadata.captureDate = $Folder.GetDetailsOf($File, 12)  # Reads photo metadata to determine capture date
         $modelProperties.metadata.capturedBy = $Folder.GetDetailsOf($File, 20)   # Reads photo metadata to determine who captured the model
+        #>
 
         $modelProperties | ConvertTo-JSON | Out-File $projectDir'\output\modelData.json'
-
-        Sleep(5)
-
     }
 } else {
     Write-Host "No directory was selected."
